bd223631231805bf94c1f7f813ea10fb2054cbe0
Tim Gathercole <gathercole.timothy@veverizon.net>
2023-09-21 17:52:39 +0000

    Update Gamemap File

--- tests/test_gamemap.py
+++ tests/test_gamemap.py
@@ -1,10 +1,9 @@
 import unittest
 
-class gamemap(unittest,TestCase):
+class gamemap(unittest,numPosition):
     def test_string(self):
         numPoistion = 100
         self.assertEqual(numPosition)
 
 if __name__ == '__main__':
     unittest.main()
-    
