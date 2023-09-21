3f76587332dc776128ccdda4408750a1d191bf2b
Tim Gathercole <gathercole.timothy@veverizon.net>
2023-09-21 18:25:04 +0000

    update character file

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -2,4 +2,8 @@ class Character:
     name = ""
 
     def __init__(self, character_name):
-        self.name = character_name
+        self.name = character_name if character_name != "" else "Builders"
+    
+    def enter_map(self, m):
+        self.map = m
+        
