50c58555452f644fb744cd3855b90470b57ccba0
Tim Gathercole <gathercole.timothy@veverizon.net>
2023-09-21 19:31:06 +0000

    test character

--- tests/test_character.py
+++ tests/test_character.py
@@ -13,3 +13,21 @@ class TestCharacterInitWithoutName(TestCase):
         ARBITRARY_NAME = " "
         testobj = Character(ARBITRARY_NAME)
         self.assertNotEqual(ARBITRARY_NAME, testobj.name)
+
+class TestChracterEnterMap(TestCase0):
+    def test_init(self):
+        ARBITRARY_NAME = ""
+        testobj = enter_map(gamemap())
+        self.assertNotEqual(None, testobj.map)
+
+class TestCharacterGetPosition(TestCase):
+    def test_init(self):
+        ARBITRARY_NAME = ""
+        testobj = Character(ARBITRARY_NAME)
+        self.assertNotEqual(Name, testobj.position)
+
+class TestCharacterMove(TestCase):
+    def test_init(self):
+        ARBITRARY_NAME = ""
+        testobj = Character(ARBITRARY_NAME)
+        self.assertNotEqual(None, testobj.move("n"))
