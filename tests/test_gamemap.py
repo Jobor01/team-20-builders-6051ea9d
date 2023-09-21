466a50d51659ea34dbc398e03cd5f2fb08219774
Tim Gathercole <gathercole.timothy@veverizon.net>
2023-09-21 17:57:00 +0000

    remove file

--- tests/test_gamemap.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class gamemap(unittest,numPosition):
-    def test_string(self):
-        numPoistion = 100
-        self.assertEqual(numPosition)
-
-if __name__ == '__main__':
-    unittest.main()
