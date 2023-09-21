150c583e78f2158ef52c1ff4be5ccdaa4d60f7d9
Tim Gathercole <gathercole.timothy@veverizon.net>
2023-09-21 16:01:02 +0000

    Test Gamemap

---
+++ tests/test_gamemap.py
@@ -0,0 +1,10 @@
+import unittest
+
+class gamemap(unittest,TestCase):
+    def test_string(self):
+        numPoistion = 100
+        self.assertEqual(numPosition)
+
+if __name__ == '__main__':
+    unittest.main()
+    
