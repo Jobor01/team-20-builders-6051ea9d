2b2c62759213f3fde4b03c4845d08d659132c49b
Todd <todd@seacorpnc.com>
2023-09-22 15:33:58 +0000

    test gamemap

--- tests/test_gamemap.py
+++ tests/test_gamemap.py
@@ -4,22 +4,22 @@ from levelup.position import position
 
 
 class TestTotalPositions(TestCase):
-        def test_init(self):
-            num_postions = 100
-            testobj = gamemap(num_postion)
-            self.assertEqual(num_postion, testobj.num_postion)
+    def test_init(self):
+        num_position = 100
+        testobj = gamemap(num_position)
+        self.assertEqual(num_position, testobj.num_position)
 
 class TestPositions(TestCase):
     def test_init(self):
         testobj = gamemap(100)
         self.assertNotEqual(None,testobj.position)
 
-class TestCalculatePostionBasedOnDirection(TestCase):
+class TestCalculatePositionBasedOnDirection(TestCase):
     def test_init(self):
         testobj = gamemap(100)
         direction = 'n'
-        startpos = testobj.position[4][5]
-        endpos = testobj.calculated_position(startpos, direction)
+        startpos = testobj.position [4][5]
+        endpos = testobj.calculate_position(startpos, direction)
         self.assertEquals(endpos.x , 5)
         self.assertEquals(endpos.y , 5)
 
@@ -28,6 +28,6 @@ class TestWalls(TestCase):
         testobj = gamemap(100)
         direction = "e"
         startpos = testobj.position[4][9]
-        endpos = testobj.calculated_position(startpos, direction)
+        endpos = testobj.calculate_position(startpos, direction)
         self.assertEquals(endpos.y, 4)
         self.assertEquals(endpos.x ,9)
