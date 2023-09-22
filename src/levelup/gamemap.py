8d00175ec4f7b3619ee92aa966975e2c632661a9
Tim Gathercole <gathercole.timothy@veverizon.net>
2023-09-22 15:09:57 +0000

    gamemap update

--- src/levelup/gamemap.py
+++ src/levelup/gamemap.py
@@ -12,4 +12,16 @@ class gamemap:
         self.grid_size = int(math.sqrt(self.num_position))
         self.position = [[position(x,y) for x in range(0, self.grid_size)]for y in range(0, self.grid_size)]
         print(self.grid_size)
-         
+
+    def calculate_position(self, startpos, movement):
+        if movement == 'n':
+            return self.position[startpos.y + 1 if startpost.y < (self.grid_size - 1) else startpos.y][startpos.x]
+        elif movement == 's':
+            return self.position[startpos.y - 1 if startpos.y > 0 else startpos.y][startpos.x]
+        elif movement == 'e':
+             return self.position[startpos.y][startpos.x + 1 if startpos.x < (self.grid_size - 1) else startpos.x]
+        elif movement == 'w':
+            return self.position[startpos.y][startpos.x - 1 if startpos.x > 0 else startpos.x]
+        else :
+            return self.position[startpos.y][startpos.x]
+            
